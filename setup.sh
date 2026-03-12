3d1cdd33d48fea68fa0fd06d688ebfd52cc7a76f
sbeyn <2492552+sbeyn@users.noreply.github.com>
2026-03-13 00:56:46 +0100

    update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt update && sudo apt install terraform
 
-mkdir /workdir && chown ubuntu:ubuntu /workdir
+mkdir /home/ubuntu/workdir && chown ubuntu:ubuntu /home/ubuntu/workdir
 
 curl -fsSL https://code-server.dev/install.sh > codeServerInstall.sh
 /bin/sh codeServerInstall.sh && systemctl enable --now code-server@root
